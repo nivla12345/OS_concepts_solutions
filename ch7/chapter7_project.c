49f0f20611c2c6896d9ff854d1db8d2968353212
Alvin Mao <asmao@andrew.cmu.edu>
2015-09-15 20:55:01 -0700

    Compiles, seg faults for some reason, trying to figure this out.

--- ch7/chapter7_project.c
+++ ch7/chapter7_project.c
@@ -28,7 +28,7 @@ typedef struct{
 } args_struct;
 
 /* the available amount of each resource */
-// TODO This may be the only resource that requires a mutex.
+// This definitely requires a mutex.
 int g_available[NUMBER_OF_RESOURCES];
 
 /*the maximum demand of each customer */
@@ -45,64 +45,74 @@ int g_need[NUMBER_OF_CUSTOMERS][NUMBER_OF_RESOURCES];
 
 // Requests resources contained in request[] for customer_num
 int request_resources(int customer_num, int request[]) {
-	
+    return 0;
 }
 
 // Requests resources contained in request[] for customer_num
 int release_resources(int customer_num, int request[]) {
-	
+    return 0;
 }
 
 void* customer_thread(void* c_void_args) {
-	while (true) {
-		
-	}
-	pthread_exit(0);
+    args_struct* as = c_void_args;
+    int customer_id = (*as).customer_id;
+
+/*    while (true) {
+        
+    }*/
+    printf("thread id: %d\n", customer_id);
+    pthread_exit(0);
 }
 
 int main(int nargs, char* argv[]) {
-	int i, j;
-    args_struct* args[NUMBER_OF_CUSTOMERS] = malloc(sizeof(args_struct) * NUMBER_OF_CUSTOMERS);
-    pthread_t thread[NUMBER_OF_CUSTOMERS];	
+    int i, j;
+    // The arguments, in this case simply the customer ID.
+    args_struct*   args[NUMBER_OF_CUSTOMERS];
+    void*        v_args[NUMBER_OF_CUSTOMERS];
+    pthread_t    thread[NUMBER_OF_CUSTOMERS];
 
-	if (nargs != NUMBER_OF_RESOURCES + 1) {
+    if (nargs != NUMBER_OF_RESOURCES + 1) {
         printf("ERROR: Needs exactly %d arguments.\n", NUMBER_OF_RESOURCES);
-        exit(1);				
-	}
+        exit(1);
+    }
 
-	// Initialize the available array.
-	for (i = 0; i < NUMBER_OF_RESOURCES; i++) {
-		g_available[i] = atoi(argv[i + 1]);
-		if (g_available < 1) {
-			printf("ERROR: The number of resources must be at least 1.\n");
-			exit(0);
-		}
-	}
-	
-	srand(time(NULL));
-	// TODO Set up argument.
-	
-	// Generate maximum, need, and allocation. The maximum is going to be <= the net number of resources.
-	for (i = 0; i < NUMBER_OF_CUSTOMERS; i++) {
-		for (j = 0; j < NUMBER_OF_RESOURCES; j++) {
-			// The +1 is to make the maximum values inclusive.
-			g_maximum[i][j]    = rand() % (g_available[j] + 1);
-			g_need   [i][j]    = g_maximum[i][j];
-			g_allocation[i][j] = 0;
-		}
-	}
+    // Initialize the available array.
+    for (i = 0; i < NUMBER_OF_RESOURCES; i++) {
+        g_available[i] = atoi(argv[i + 1]);
 
-	// Construct and spawn threads
+        if (g_available[i] < 1) {
+	    printf("ERROR: The number of resources must be at least 1.\n");
+            exit(0);
+        }
+    }
+
+    srand(time(NULL));
+
+    // Generate maximum, need, and allocation. The maximum is going to be <= the net number of resources.
     for (i = 0; i < NUMBER_OF_CUSTOMERS; i++) {
-        if (pthread_create(&thread[i], NULL, &customer_thread, (void*)args[i]) != 0) {
+        for (j = 0; j < NUMBER_OF_RESOURCES; j++) {
+            // The +1 is to make the maximum values inclusive.
+            g_maximum[i][j]    = rand() % (g_available[j] + 1);
+            g_need   [i][j]    = g_maximum[i][j];
+            g_allocation[i][j] = 0;
+        }
+    }
+
+    // Construct and spawn arguments and threads
+    for (i = 0; i < NUMBER_OF_CUSTOMERS; i++) {
+        (*args[i]).customer_id = i;
+        v_args[i] = (void*)args[i];
+        if (pthread_create(&thread[i], NULL, &customer_thread, v_args[i]) != 0) {
             printf("ERROR: Monte Carlo thread created incorrectly.\n");
             exit(0);
         }
     }
 
-	// Close and dispose of everything. We won't ever reach this because our threads go on forever.
+
+    printf("About to join.\n");
+    // Close and dispose of everything. We won't ever reach this because our threads go on forever.
     for (i = 0; i < NUMBER_OF_CUSTOMERS; i++)
         pthread_join(thread[i], NULL);
-    free(args);
+    printf("Completed join.\n");
     return 0;
 }
