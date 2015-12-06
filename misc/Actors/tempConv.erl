4c5629c6457c3e8a102a77abbd6df3f9e0f1b86c
nivla12345 <nivlamao2@gmail.com>
2015-12-05 18:16:23 -0800

    Update all

---
+++ misc/Actors/tempConv.erl
@@ -0,0 +1,17 @@
+-module(tempConv).
+-export([temperatureConverter/1]).
+
+temperatureConverter() ->
+ receive
+ {toF, C} ->
+ io:format("~p C is ~p F~n", [C, 32+C*9/5]),
+ temperatureConverter();
+ {toC, F} ->
+ io:format("~p F is ~p C~n", [F, (F-32)*5/9]),
+ temperatureConverter();
+ {stop} ->
+ io:format("Stopping~n");
+ Other ->
+ io:format("Unknown: ~p~n", [Other]),
+ temperatureConverter()
+ end.
